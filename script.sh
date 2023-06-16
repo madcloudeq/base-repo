86fed4f9049b21882bd8a830ccdf4118485e3af9
madcloudeq <madhav.ahuja@cloudeq.com>
2023-06-16 15:55:41 +0000

    testing file upload

---
+++ script.sh
@@ -0,0 +1,33 @@
+repo_url="https://github.com/username/repository.git"  # Replace with your repository URL
+branch="main"  # Replace with your branch name
+file_path="path/to/file.txt"  # Replace with the path to your file
+
+ 
+
+# Set up Git credentials
+git config --global credential.helper store
+git config --global user.email "madhav.ahuja@cloudeq.com"  # Replace with your email address
+git config --global user.name "madcloudeq"  # Replace with your name
+
+ 
+
+# Clone the repository
+git clone https://github.com/madcloudeq/cross-repo-trigger.git temp_repo
+cd temp_repo
+
+ 
+
+# Checkout the branch
+git checkout main
+
+ 
+
+# Copy the file to the repository
+cp ../mad.txt .
+
+ 
+
+# Add, commit, and push the changes
+git add .
+git commit -m "Adding file"
+git push origin main
